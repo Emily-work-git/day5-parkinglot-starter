b69184b2e4f13d029eb677bec4163875f6bc4d6b
Emily Ng <ngem3@oocl.com>
2024-11-22 15:47:35 +0800

    [Story3] feat: Case 3, return correct car for each ticket when fetch car by two different ticket

--- src/test/java/com/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -27,4 +27,19 @@ public class ParkingBoyTest {
         // Then
         assertEquals(car,fetchedCar);
     }
+    @Test
+    public void should_return_the_corresponding_ticket_of_a_car_when_fetch_given_two_fetch_request() {
+        // Given
+        ParkingBoy parkingBoy = new ParkingBoy();
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Ticket ticket1 = parkingBoy.park(car1);
+        Ticket ticket2 = parkingBoy.park(car2);
+        // When
+        Car fetchedCar1 = parkingBoy.fetch(ticket1);
+        Car fetchedCar2 = parkingBoy.fetch(ticket2);
+        // Then
+        assertEquals(car1, fetchedCar1);
+        assertEquals(car2, fetchedCar2);
+    }
 }
