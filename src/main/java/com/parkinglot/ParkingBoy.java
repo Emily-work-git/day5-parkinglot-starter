05a8fc9bb74d30aebc37bb287cf83599e4102fb1
Emily Ng <ngem3@oocl.com>
2024-11-22 15:43:31 +0800

    [Story3] feat: Case 2, return correct car when a parkingBoy fetch car by a ticket

--- src/main/java/com/parkinglot/ParkingBoy.java
+++ src/main/java/com/parkinglot/ParkingBoy.java
@@ -7,4 +7,7 @@ public class ParkingBoy extends ParkingLot{
     public Ticket park (Car car){
         return parkingLot.park(car);
     }
+    public Car fetch (Ticket ticket){
+        return parkingLot.fetch(ticket);
+    }
 }
--- src/test/java/com/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -2,11 +2,12 @@ package com.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ParkingBoyTest {
     @Test
-    public void should_return_ticket_when_park_given_a_car_and_a_parking_lot(){
+    public void should_return_ticket_when_park_given_a_car(){
         // Given
         ParkingBoy parkingBoy = new ParkingBoy();
         Car car = new Car();
@@ -14,5 +15,16 @@ public class ParkingBoyTest {
         Ticket ticket = parkingBoy.park(car);
         // Then
         assertNotNull(ticket);
+    }
+    @Test
+    public void should_return_car_when_fetch_given_a_car(){
+        // Given
+        ParkingBoy parkingBoy = new ParkingBoy();
+        Car car = new Car();
+        Ticket ticket = parkingBoy.park(car);
+        // When
+        Car fetchedCar = parkingBoy.fetch(ticket);
+        // Then
+        assertEquals(car,fetchedCar);
     }
 }
