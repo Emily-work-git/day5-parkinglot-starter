8b88a6f1ffde73602dfba65ecbf2f41bdb33c2c4
Emily Ng <ngem3@oocl.com>
2024-11-24 01:39:09 +0800

    [Story6] feat: Case 2 should return second parking lot when park given the second parking lot has larger available position rate

--- src/test/java/com/parkinglot/SuperParkingBoyTest.java
+++ src/test/java/com/parkinglot/SuperParkingBoyTest.java
@@ -21,4 +21,19 @@ public class SuperParkingBoyTest {
         ParkingLot parkedParkingLot = superParkingBoy.getParkingLotByTicket(ticket);
         assertEquals(parkingLot1,parkedParkingLot);
     }
+    @Test
+    public void should_return_second_parking_lot_when_park_given_the_second_parking_lot_has_larger_available_position_rate(){
+        // Given
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy();
+        ParkingLot parkingLot1 = new ParkingLot();
+        ParkingLot parkingLot2 = new ParkingLot(20);
+        smartParkingBoy.addParkingLot(parkingLot1);
+        smartParkingBoy.addParkingLot(parkingLot2);
+        Car car = new Car();
+        // When
+        Ticket ticket = smartParkingBoy.park(car);
+        // Then
+        ParkingLot parkedParkingLot = smartParkingBoy.getParkingLotByTicket(ticket);
+        assertEquals(parkingLot2,parkedParkingLot);
+    }
 }
